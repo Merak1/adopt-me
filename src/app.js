23637cde6fcde2b3b0554f4e09766cf195bb9e71
Merak1 <eddie_comes_for_you@hotmail.es>
2019-07-08 12:46:11 -0500

    JSX integration

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
-// const Pet = (props) => {
-//     return React.createElement("div", {},
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed),
-//     )
-// }
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {},
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    )
-}
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import Pet from './Pet';
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet, {
-            name: "Nemo",
-            animal: "Fish",
-            breed: "Clow"
-        }),
-        React.createElement(Pet, {
-            name: "Clifford",
-            animal: "Dog",
-            breed: "Red"
-        }),
-        React.createElement(Pet, {
-            name: "Kazumi",
-            animal: "cat",
-            breed: "kira kira doki doki"
-        }),
-    );
+    // return React.createElement(
+    //     "div",
+    //     {},
+    //     React.createElement("h1", {}, "Adopt me!"),
+    //     React.createElement(Pet, {
+    //         name: "Nemo",
+    //         animal: "Fish",
+    //         breed: "Clow"
+    //     }),
+    //     React.createElement(Pet, {
+    //         name: "Clifford",
+    //         animal: "Dog",
+    //         breed: "Red"
+    //     }),
+    //     React.createElement(Pet, {
+    //         name: "Kazumi",
+    //         animal: "cat",
+    //         breed: "kira kira doki doki"
+    //     }),
+    // );
+
+    return (
+        <div>
+            <h1>Adopt me!</h1>
+            <Pet name="Nemo" animal="Fish" breed="Clow" />
+            <Pet name="Clifford" animal="Dog" breed="Red" />
+            <Pet name="Kazumi" animal="Cat" breed="kira kira doki dokiiiii" />
+        </div>
+    )
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
