4bf500086a622a9f3691645acfbf851f1a7619fb
Merak1 <eddie_comes_for_you@hotmail.es>
2019-11-04 18:23:41 -0600

    added build

---
+++ build/precache-manifest.193e8d496aae60f8efc70b8af906539d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219bf5adb57903225c75218d3346f793",
+    "url": "/adopt-me/index.html"
+  },
+  {
+    "revision": "e054f95dd2c80d64ff1b",
+    "url": "/adopt-me/static/css/main.8b32ab18.chunk.css"
+  },
+  {
+    "revision": "2b9ea9ebf5c7881ff595",
+    "url": "/adopt-me/static/js/2.edfb6f00.chunk.js"
+  },
+  {
+    "revision": "e054f95dd2c80d64ff1b",
+    "url": "/adopt-me/static/js/main.fcdb491d.chunk.js"
+  },
+  {
+    "revision": "c73eb3029d5c40c5c6ff",
+    "url": "/adopt-me/static/js/runtime-main.7e4de8fb.js"
+  }
+]);
